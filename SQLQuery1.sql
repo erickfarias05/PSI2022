f273aac609fd89a4b5731ac64f4a6d8ace0fd8f2
Erick Santos Umbelino de Farias <erick.f@academico.ifrn.edu.br>
2022-10-17 10:14:35 -0300

    Adicionar arquivos de projeto.

---
+++ SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿select * from Categorias
+
+select * from Produtoes
+
+insert into Categorias values ('Software')
+
+insert into Fabricantes values ('Microsoft')
+
+insert into Produtoes values ('Windows',1,1)
